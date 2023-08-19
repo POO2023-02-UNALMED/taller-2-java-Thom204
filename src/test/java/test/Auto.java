7bbe60c9b08224127e74c178f73a38ee95752e90
Thom204 <tmonnier@unal.edu.co>
2023-08-18 22:09:59 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -2,19 +2,19 @@ package test;
 
 public class Auto {
 	String modelo;
-	int percio;
-	Asiento[] asiento;
+	int precio;
+	Asiento[] asientos;
 	String marca;
 	Motor motor;
 	int registro;
 	static int cantidadCreados;
 
 	int cantidadAsientos(){
-		return this.asiento.length;
+		return this.asientos.length;
 	}
 	String verificarIntegridad() {
 		String Result = null;
-		for (Asiento i: asiento) {
+		for (Asiento i: asientos) {
 			if (!(this.registro== motor.registro && this.registro==i.registro)) {
 				Result="Las piezas no son originales";
 				break;
